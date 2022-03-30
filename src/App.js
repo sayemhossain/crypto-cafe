05965465b4591549920bee033a5dd6a71a1f6436
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-30 15:55:17 +0600

    reacr router install done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold bg-slate-600 text-center mt-10">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
