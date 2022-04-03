46bb1800f980d4469e6053641b8847bdf822053f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-03 15:01:09 +0600

    spinner problem solving

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from "react";
 import AllCoin from "../AllCoin/AllCoin";
+import "../Spinner/Spinner";
 
 const Coins = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     )
       .then((res) => res.json())
       .then((data) => setCoins(data));
+    setLoading(false);
   }, []);
   return (
-    <div className="bg-slate-200">
-      <div className="h-[100%] className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
-        <div className="text-center py-6">
-          <h1 className="text-xl md:text-3xl font-bold text-gray-600 mb-2">
-            Available Crypto Currencies
-          </h1>
-          <p className="text-gray-500">Total coins: {coins.length}</p>
-        </div>
-        <div className="flex justify-center">
-          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center">
-            {coins.map((coin) => (
-              <AllCoin key={coin.id} coin={coin}></AllCoin>
-            ))}
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="bg-slate-200">
+          <div className="h-[100%] className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
+            <div className="text-center py-6">
+              <h1 className="text-xl md:text-3xl font-bold text-gray-600 mb-2">
+                Available Crypto Currencies
+              </h1>
+              <p className="text-gray-500">Total coins: {coins.length}</p>
+            </div>
+            <div className="flex justify-center">
+              <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center">
+                {coins.map((coin) => (
+                  <AllCoin key={coin.id} coin={coin}></AllCoin>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
