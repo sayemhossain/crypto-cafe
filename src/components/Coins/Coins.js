0a3f85adb59e26a5707352abb9a9c93a249097d4
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-03 15:05:25 +0600

    spinner addition done

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AllCoin from "../AllCoin/AllCoin";
-import "../Spinner/Spinner";
+import Spinner from "../Spinner/Spinner";
 
 const Coins = () => {
   const [coins, setCoins] = useState([]);
@@ -11,13 +11,15 @@ const Coins = () => {
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     )
       .then((res) => res.json())
-      .then((data) => setCoins(data));
-    setLoading(false);
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+      });
   }, []);
   return (
     <>
       {loading ? (
-        <Spinner />
+        <Spinner></Spinner>
       ) : (
         <div className="bg-slate-200">
           <div className="h-[100%] className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
